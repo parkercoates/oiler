217a02bae49aa83388fbe7df496723ba238a89b0
Parker Coates <parker.coates@kdemail.net>
2011-11-20 06:19:48 -0500

    Rework euler043.hs to be much more efficient.
    Runtime from 2.6s to 0.06s.

--- euler043.hs
+++ euler043.hs
@@ -1,15 +1,28 @@
-import Data.List (tails, permutations)
-import EulerUtils (divides, fromDigits, isPrime)
+import Data.List (delete)
+import EulerUtils (divides, fromDigits)
 
-propertyCheck (_:a:b:c:d:e:f:g:h:i:[]) = 
-                                         17 `divides` fromDigits [g,h,i] &&
-                                         13 `divides` fromDigits [f,g,h] &&
-                                         11 `divides` fromDigits [e,f,g] &&
-                                         7 `divides` fromDigits [d,e,f] &&
-                                         5 `divides` fromDigits [c,d,e] &&
-                                         3 `divides` fromDigits [b,c,d] &&
-                                         2 `divides` fromDigits [a,b,c]
+check = check2
+   where check2  ds@(_:a:b:c:_) = dividesDigits 2 a b c && check3 ds
+         check2  _ = True
+         check3  ds@(_:_:a:b:c:_) = dividesDigits 3 a b c && check5 ds
+         check3  _ = True
+         check5  ds@(_:_:_:a:b:c:_) = dividesDigits 5 a b c && check7 ds
+         check5  _ = True
+         check7  ds@(_:_:_:_:a:b:c:_) = dividesDigits 7 a b c && check11 ds
+         check7  _ = True
+         check11 ds@(_:_:_:_:_:a:b:c:_) = dividesDigits 11 a b c && check13 ds
+         check11 _ = True
+         check13 ds@(_:_:_:_:_:_:a:b:c:_) = dividesDigits 13 a b c && check17 ds
+         check13 _ = True
+         check17 ds@(_:_:_:_:_:_:_:a:b:c:_) = dividesDigits 17 a b c
+         check17 _ = True
+         dividesDigits n a b c = n `divides` (a * 100 + b * 10 + c)
 
+perms r = perms' [] r
+   where perms' a [] = [a]
+         perms' a r = concat [ perms' a' r' | x <- r
+                                            , let a' = a ++ [x]
+                                            , check a'
+                                            , let r' = delete x r ]
 
-main = print . sum . map fromDigits . filter propertyCheck . permutations $ [0..9]
-
+main = print . sum . map fromDigits . perms $ [0..9]
