624d3341f54d9b1b9d5612fdc212380f19b06132
Parker Coates <coates@qps.nl>
2013-03-25 00:06:16 -0300

    Further optimise my Haskell solution to Problem #55.
    I can't really stand when my Python solution beats my Haskell one, so I
    had to go back an optimise. Given how ugly the digitAdd function is, I
    was quite surprised to see it perform so much better.

--- euler055.hs
+++ euler055.hs
@@ -6,9 +6,24 @@ import EulerUtils (digits, revDigits, fromDigits)
 -- isLychrel = not . any isPalindromic . take 50 . tail . iterate next
 --     where next n = n + fromDigits (revDigits n)
 
-isPalindromic ds = ds == reverse ds
 
-isLychrel = not . any isPalindromic . take 50 . tail . iterate next . digits
-    where next ds = digits $ fromDigits ds + fromDigits (reverse ds)
+-- isPalindromic ds = ds == reverse ds
+--
+-- isLychrel = not . any isPalindromic . take 50 . tail . iterate next . digits
+--     where next ds = digits $ fromDigits ds + fromDigits (reverse ds)
+
+
+isPalindromic = uncurry (==)
+
+isLychrel = not . any isPalindromic . take 50 . tail . iterate next . reverseJoin . revDigits
+    where reverseJoin ds = (reverse ds, ds)
+          next (ds, rds) = reverseJoin $ digitAdd 0 ds rds
+          digitAdd 0 []     []     = []
+          digitAdd c []     []     = [c]
+          digitAdd c ds     []     = digitAdd c ds [0]
+          digitAdd c []     rs     = digitAdd c [0] rs
+          digitAdd c (d:ds) (r:rs) = let (c',v) = (d + r + c) `quotRem` 10
+                                     in v:digitAdd c' ds rs
+
 
 main = print . length . filter isLychrel $ [1..10000]
